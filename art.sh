d7a0754998aaa2c81332bc148524304261980fa5
Kazi Rifat Morshed <67235895+KaziRifatMorshed@users.noreply.github.com>
2023-08-26 20:26:47 +0600

    uploaded files to github from desktop

---
+++ art.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+bold=`echo -en "\e[1m"`
+underline=`echo -en "\e[4m"`
+dim=`echo -en "\e[2m"`
+strickthrough=`echo -en "\e[9m"`
+blink=`echo -en "\e[5m"`
+reverse=`echo -en "\e[7m"`
+hidden=`echo -en "\e[8m"`
+normal=`echo -en "\e[0m"`
+black=`echo -en "\e[30m"`
+red=`echo -en "\e[31m"`
+green=`echo -en "\e[32m"`
+orange=`echo -en "\e[33m"`
+blue=`echo -en "\e[34m"`
+purple=`echo -en "\e[35m"`
+aqua=`echo -en "\e[36m"`
+gray=`echo -en "\e[37m"`
+darkgray=`echo -en "\e[90m"`
+lightred=`echo -en "\e[91m"`
+lightgreen=`echo -en "\e[92m"`
+lightyellow=`echo -en "\e[93m"`
+lightblue=`echo -en "\e[94m"`
+lightpurple=`echo -en "\e[95m"`
+lightaqua=`echo -en "\e[96m"`
+white=`echo -en "\e[97m"`
+default=`echo -en "\e[39m"`
+BLACK=`echo -en "\e[40m"`
+RED=`echo -en "\e[41m"`
+GREEN=`echo -en "\e[42m"`
+ORANGE=`echo -en "\e[43m"`
+BLUE=`echo -en "\e[44m"`
+PURPLE=`echo -en "\e[45m"`
+AQUA=`echo -en "\e[46m"`
+GRAY=`echo -en "\e[47m"`
+DARKGRAY=`echo -en "\e[100m"`
+LIGHTRED=`echo -en "\e[101m"`
+LIGHTGREEN=`echo -en "\e[102m"`
+LIGHTYELLOW=`echo -en "\e[103m"`
+LIGHTBLUE=`echo -en "\e[104m"`
+LIGHTPURPLE=`echo -en "\e[105m"`
+LIGHTAQUA=`echo -en "\e[106m"`
+WHITE=`echo -en "\e[107m"`
+DEFAULT=`echo -en "\e[49m"`;
+
+echo "                                                                    +::::::::::::::::::::::::::::::::::::::+"
+echo " Bappi               Seam                                           + :..................................: +"
+echo " Munna             Siam                                             + :            .. : :. .  .          : +"
+echo " Rahul           Tuly                                               + :      .   ::..:.:... ::.  ..      : +"
+echo " Rashi        Toma                                                  + :       ......:.:-:.:. .::.        : +"
+echo " Reyad      Mohi                 Mahi\       Musfika                + :  :::..........:--:.......::::::  : +"
+echo " Rimon   Kazi                    Munna       Bappi/                 + :...............---:...............: +"
+echo " Sahan Emon                      Rashi       Rahul                  + :             ...::...             : +"
+echo " Mahin Faisal                    Rimon       Reyad                  + :               ....               : +"
+echo " Sajal Ishrak                    Sakib       Sajal                  + : -------:     .-         :-------.: +"
+echo " Sakib    Mahfuz                 Sobuj       Shams                  + : ---:.:-:   :.-- :=.     :-:::::-.: +"
+echo " Shams      Kawsar               Tamal       Summa                  + : :.: ..-:   -++=-:       :-... :-.: +"
+echo " Sobuj         Durjay            Sabbir      Turjo                  + : : .. .::    =+=         :- . ::-.: +"
+echo " Summa           Moinul          Shibly      Sahan                  + : :-:...-:    ++:  ...    :-....:-.: +"
+echo " Tamal             Marufa        Jagaran|||||Toma                   + : --:. .-:   .++:-+++++:  :-. :---.: +"
+echo " Turjo               Pranto          Mahazabin                      + : -:....-:   :+++++++++   :-.  :--.: +"
+echo "                                                                    + : --: :.-:   .++++++++:   ::.....-.: +"
+echo "                                                                    = : ---...--    +++-:-++.   --.:::-- : ="
+echo "       Welcome 33th batch of KU CSE !                               -:..::.....-:   =+-   ++.  .-:.:..:: ..-"
+echo "                                                                    + : --: :.-:   .++++++++:   ::.....-.: +"
+echo "                                                                    = : ---...--    +++-:-++.   --.:::-- : ="
+echo "     Mustafiz          Sabbir|Mohi       Mahfuz|Mashrafi            -:..::.....-:   =+-   ++.  .-:.:..:: ..-"
+echo " Jannatul|Jagaran    Mashrafi|Musfika    Jannatul|Ishrak             = : -:....--:  :+-   +:  .-:. ..:-.: =."
+echo " Emon        Kazi    Saadiq      Seam    Faisal|Mustafiz             :: ..------.....-.  .-..:::.. ---.. :- "
+echo " Seam                Pranto                                           -: ..---:..::::::::::.:.. .::--.. :-  "
+echo " Mohi                  Moinul                                          :- ..:-.:: .  :.:... :: ::.::.. --   "
+echo " Toma                    Durjay          Kawsar|Moinul        Musfika\    Jagaran     .....: :---:....-.    "
+echo " Tuly                      Kawsar        Durjay|Marufa      Seam   Emon   Sabbir|\    -:.. -:::.....-:      "
+echo " Emon                        Durjay                               Mohi        Pranto  ...........:-.        "
+echo " Siam                         Marufa                             Rahul    Shibly||||  ..:::::::.           "
+echo " Kazi        Emon    Toma       Shibly   Jannatul|Faisal       Munna          Marufa" 
+echo " Mashrafi|Musfika    Jagaran|Mustafiz    Ishrak|Shahriar      Bappi       Saadiq|/"   
+echo "     Shahriar            Mahazabin       Mustafiz|Mahfuz     Mahazabin|   Musfika"    
+echo "${aqua} _     ___    __    ___   __  _     _     ___    __    __       __    __  _   __      _     _   _   _   ___  "
+echo "${aqua}| |   | __|  /  \  | _ \ |  \| |   | |   | __|  /  \  | _\     /  \  |  \| | | _\    | |   | | | \ / | | __| "
+echo "${aqua}| |_  | _|  | /\ | | v / | | ' |   | |_  | _|  | /\ | | v |   | /\ | | | ' | | v |   | |_  | |  \ V /  | _|  "
+echo "${aqua}|___| |___| |_||_| |_|_\ |_|\__|   |___| |___| |_||_| |__/    |_||_| |_|\__| |__/    |___| |_|   \_/   |___| "
